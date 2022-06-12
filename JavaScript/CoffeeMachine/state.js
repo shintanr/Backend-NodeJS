843abebfcdcfdc11cb91e3f58ea70bef6d099d9b
shintanr <shintanurrohmah143@gmail.com>
2022-06-12 18:46:54 +0700

    upload file state.js

---
+++ JavaScript/CoffeeMachine/state.js
@@ -0,0 +1,10 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200,
+};
+
+module.exports = coffeeStock;
+// object coffeeStock ditetapkan sebagai nilai dari module.export. nantinya bisa digunakan pada berkas js lainnya
+
+console.log(module);
