e9894328c0fdae9cb33f14aa706ae87e9ebab2bc
shintanr <shintanurrohmah143@gmail.com>
2022-06-12 18:42:13 +0700

    add index.js

---
+++ JavaScript/CoffeeMachine/index.js
@@ -0,0 +1,11 @@
+const coffeeStock = require("./state");
+
+// nama variabel bebas, require untuk memberikan parameter lokasi berkas state.js
+
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
