74cd1eec936dba1ad3da0167f4a90bcb39e71b0c
shintanr <shintanurrohmah143@gmail.com>
2022-06-13 03:39:34 +0700

    upload quiz module

---
+++ JavaScript/kuis-module/main.js
@@ -0,0 +1,22 @@
+const Tiger = require("./tiger.js");
+const Wolf = require("./wolf.js");
+
+const fight = (tiger, wolf) => {
+    if (tiger.strength > wolf.strength) {
+        return tiger.growl();
+    }
+
+    if (wolf.strength > tiger.strength) {
+        return wolf.howl();
+    }
+
+    return "Harimau dan serigala sama-sama kuat";
+};
+
+const myTiger = new Tiger();
+const myWolf = new Wolf();
+const result = fight(myTiger, myWolf);
+
+module.exports = { fight, myTiger, myWolf, result };
+
+// MENGGUNAKAN EXPORTS MODULE
