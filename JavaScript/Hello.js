f0bc3ca3bfdfde4f0c1b148a66531e7eb7a486ed
shintanr <shintanurrohmah143@gmail.com>
2022-06-16 21:00:09 +0700

    upload files

--- JavaScript/Hello.js
+++ JavaScript/Hello.js
@@ -1,3 +1,29 @@
 console.log("hello");
 
 console.log("Shinta");
+
+let x;
+x = 7;
+x = "JS is great";
+console.log(x);
+
+function calculate(value) {
+    return value < 2 ? value : calculate(value - 1) + calculate(value - 2);
+}
+
+console.log(calculate(3));
+
+let myString = "";
+
+try {
+    myString += "a";
+    throw Error();
+} catch (e) {
+    myString += "b";
+} finally {
+    myString += "c";
+    throw Error();
+    myString += "d";
+}
+
+console.log(myString);
---
+++ JavaScript/latihantesting/__tests__/gradeCalculations.test.js
@@ -0,0 +1,11 @@
+const { averageExams } = require("../gradeCalculations");
+
+test("it should return exact average", () => {
+    const listValueOfExams = [80, 100, 100, 80];
+    expect(averageExams(listValueOfExams)).toEqual(90);
+});
+
+test("it should handle non-number ", () => {
+    const listValueOfExams = [80, "a", "100", 80];
+    expect(() => averageExams(listValueOfExams)).toThrow();
+});
